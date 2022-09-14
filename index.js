3b9d4862065eddf0fe5a33dca14a5150db608f42
Sirjamo1 <jam_how@hotmail.com>
2022-09-14 13:13:10 +1000

    form finished

---
+++ index.js
@@ -0,0 +1,9 @@
+export default function onChange() {
+    const password = document.querySelector("input[name=password]");
+    const confirm = document.querySelector("input[name=confirm]");
+    if (confirm.value === password.value) {
+        confirm.setCustomValidity("");
+    } else {
+        confirm.setCustomValidity("Passwords do not match");
+    }
+}
